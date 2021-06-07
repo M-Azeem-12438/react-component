da3d63375c4bed3ece08d44330e2b542ec9b14e2
M-Azeem-12438 <azeemhaleem1947@gmail.com>
2021-06-07 00:33:41 -0700

    component

---
+++ react1/src/components/header.js
@@ -0,0 +1,11 @@
+import './header.css'
+
+function Header() {
+    return(
+        <div id="aaa">
+            <h1>kjiohjiojio</h1>
+            
+            </div>
+    )
+}
+export default Header;
